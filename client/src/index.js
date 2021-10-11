7f038dd7fa99df45f1cb202ee71f6e906d8baa85
guess01325 <guess01325@gmail.com>
2021-10-11 10:13:54 -0400

    deployed

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
@@ -20,4 +20,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-// reportWebVitals();
