475c05c15e5b6e6bd894cf5b1333967929cdc8dd
guess01325 <guess01325@gmail.com>
2024-02-22 21:32:20 -0500

    updated file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quad-pack.netlify.app'
+    origins "*"
+    # 'https://quad-pack.netlify.app'
 
     resource '*',
       headers: :any,
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_025818) do
-
+ActiveRecord::Schema[7.1].define(version: 2021_10_07_025818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2021_10_07_025818) do
     t.string "venue"
     t.string "city"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 2021_10_07_025818) do
     t.string "amount"
     t.bigint "user_id", null: false
     t.bigint "event_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_luggages_on_event_id"
     t.index ["user_id"], name: "index_luggages_on_user_id"
   end
@@ -42,8 +41,8 @@ ActiveRecord::Schema.define(version: 2021_10_07_025818) do
     t.string "email"
     t.string "password_digest"
     t.string "organization"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "username"
   end
 
